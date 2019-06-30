b9b4c56f8bca82371f6a37efbf0c9f67acc79bd7
Dung-Ru Tsai <ldotrg@hotmail.com>
2019-06-30 13:32:37 +0800

    Add the so lib makefile template

--- modules/hello_world/hello.c
+++ modules/hello_world/hello.c
@@ -3,3 +3,7 @@
 void say_hello(void) {
     printf("[dungru:%d:%s] \n", __LINE__, __FUNCTION__);
 }
+
+void say_hello_1(void) {
+    printf("[dungru:%d:%s] \n", __LINE__, __FUNCTION__);
+}
--- modules/hello_world/hello.h
+++ modules/hello_world/hello.h
@@ -1,2 +1,6 @@
 
 void say_hello(void);
+void say_hello_1(void);
+#if 0
+hello56557788
+#endif
