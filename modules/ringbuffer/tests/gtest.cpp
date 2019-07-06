f7bc731958e2ec818687b74b7dee7e5823be782b
Dung-Ru Tsai <ldotrg@hotmail.com>
2019-07-06 14:12:42 +0800

    Define the trival test

--- modules/ringbuffer/tests/gtest.cpp
+++ modules/ringbuffer/tests/gtest.cpp
@@ -2,10 +2,16 @@
 #include "integration_test_cases.cpp"
 #include <gtest/gtest.h>
  
-TEST(ringbuffer_basic_test, PositiveNos) {
+TEST(ringbuffer_basic_test, Trivial) {
     struct ringbuffer_t rb;
     ASSERT_EQ(0, ringbuffer_basic_test());
     ASSERT_EQ(0, rb_init(&rb, 8));
+}
+
+TEST(rb_init, Trivial) {
+    struct ringbuffer_t rb;
+    ASSERT_EQ(0, rb_init(&rb, 8));
+    ASSERT_EQ(-1, rb_init(&rb, 7));
 }
  
 int main(int argc, char **argv) {
