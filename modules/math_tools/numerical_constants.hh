8d36fd8d59eacb1133789d47e0139cf9dd9ecab2
Dung-Ru Tsai <ldotrg@hotmail.com>
2019-08-12 21:07:48 +0800

    Modules: Add math tools

---
+++ modules/math_tools/numerical_constants.hh
@@ -0,0 +1,18 @@
+/**
+ * \file numerical_constants.h
+ *
+ * \brief Defines all global constant parameters for HYPER simulation
+ */
+
+#ifndef __NUMERICAL_CONSTANTS_HH__
+#define __NUMERICAL_CONSTANTS_HH__
+
+
+// numerical constants
+double const PI = 3.1415926536;  ///< circumference of unit diameter circle
+double const EPS = 1.e-10;       ///< machine precision error (type double)
+double const SMALL = 1e-7;       ///< small real number
+int const ILARGE = 9999;         ///< large integer number
+double const LARGE = 1e10;       ///< large real number (type double)
+
+#endif  //  __NUMERICAL_CONSTANTS_HH__
