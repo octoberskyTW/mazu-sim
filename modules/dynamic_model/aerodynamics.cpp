98b353de9d20e86bffc48b4196519b7bb7c18091
Dung-Ru Tsai <ldotrg@hotmail.com>
2019-08-21 21:13:53 +0800

    Create aero dynamic

---
+++ modules/dynamic_model/aerodynamics.cpp
@@ -0,0 +1,98 @@
+
+#include <iostream>
+#include "aerodynamics.hh"
+AeroDynamics::AeroDynamics() {}
+
+void AeroDynamics::load_aerotable(const char *filename)
+{
+    aerotable = Datadeck(filename);
+}
+
+void AeroDynamics::algorithm(LaunchVehicle *VehicleIn)
+{
+    /* only calculate when rocket liftoff */
+    Aerodynamics_var *A;
+    DM_var *D;
+    EarthEnvironment_var *E;
+    Prop_var *P;
+    P = VehicleIn->Prop;
+    D = VehicleIn->DM;
+    A = VehicleIn->Aero;
+    E = VehicleIn->Env;
+
+    int liftoff = D->liftoff;
+    int thrust_on = 0;
+    // data_exchang->hget("liftoff", &liftoff);
+
+    if (liftoff == 1) {
+        double vmach = E->vmach;
+        double dvba = E->dvba;
+        double alppx = D->alppx;
+        double phipx = D->phipx;
+        double alt = D->alt;
+        // data_exchang->hget("alppx", &alppx);
+        // data_exchang->hget("phipx", &phipx);
+        // data_exchang->hget("vmach", &vmach);
+        // data_exchang->hget("dvba", &dvba);
+        // data_exchang->hget("alt", &alt);
+
+        arma::vec3 WBIB = D->WBIB;
+        arma::vec3 WBEB = D->WBEB;
+        arma::vec3 XCG = P->XCG;
+        // data_exchang->hget("XCG", XCG);
+        // data_exchang->hget("WBEB", WBEB);
+        // data_exchang->hget("WBIB", WBIB);
+
+        enum THRUST_TYPE thrust_state = VehicleIn->Prop->thrust_state;
+        // double vmass  = grab_vmass();
+
+        //  transforming body rates from body -> aeroballistic coord.
+        double phip = phipx * RAD;
+        double cphip = cos(phip);
+        double sphip = sin(phip);
+        double qqax = WBEB(1) * cphip - WBEB(2) * sphip;
+        double rrax = WBEB(1) * sphip + WBEB(2) * cphip;
+
+        // looking up axial force coefficients
+        A->ca0 = aerotable.look_up("ca0_vs_mach", vmach, 0);
+        A->caa = aerotable.look_up("caa_vs_mach", vmach, 0);
+        A->ca0b = aerotable.look_up("ca0b_vs_mach", vmach, 0);
+
+        // axial force coefficient
+        if (thrust_state = NO_THRUST)
+            thrust_on = 1;
+        A->ca = A->ca0 + A->caa * alppx + thrust_on * A->ca0b;
+
+        // looking up normal force coefficients in aeroballistic coord
+        A->cn0 = aerotable.look_up("cn0_vs_mach_alpha", vmach, alppx, 0);
+        // normal force coefficient
+        A->cna = A->cn0;
+
+        // looking up pitching moment coefficients in aeroballistic coord
+        A->clm0 = aerotable.look_up("clm0_vs_mach_alpha", vmach, alppx, 0);
+        A->clmq = aerotable.look_up("clmq_vs_mach", vmach, 0);
+        // pitching moment coefficient
+        double clmaref = A->clm0 + A->clmq * qqax * A->refd / (2. * dvba);
+        A->clma = clmaref - A->cna * (A->XCP(0) + XCG(0)) / A->refd;
+
+        // converting force and moment coeff to body axes
+        // force coefficients in body axes
+        A->cx = -A->ca;
+        A->cy = -A->cna * sphip;
+        A->cz = -A->cna * cphip;
+        // moment coefficient in body axes
+        A->cll = 0;
+        A->clm = A->clma * cphip;
+        A->cln = -A->clma * sphip;
+    }
+
+    // data_exchang->hset("refa", A->refa);
+    // data_exchang->hset("refd", A->refd);
+    // data_exchang->hset("cx", A->cx);
+    // data_exchang->hset("cy", A->cy);
+    // data_exchang->hset("cz", A->cz);
+    // data_exchang->hset("cll", A->cll);
+    // data_exchang->hset("clm", A->clm);
+    // data_exchang->hset("cln", A->cln);
+    // data_exchang->hset("XCP", A->XCP);
+}
---
+++ modules/dynamic_model/aerodynamics.hh
@@ -0,0 +1,38 @@
+#ifndef __AERODYNAMICS_HH__
+#define __AERODYNAMICS_HH__
+/********************************* TRICK HEADER *******************************
+PURPOSE:
+      (Describe the AERODYNAMICS Module On Board)
+LIBRARY DEPENDENCY:
+      ((../src/Aerodynamics.cpp))
+PROGRAMMERS:
+      (((Chun-Hsu Lai) () () () ))
+*******************************************************************************/
+#include <armadillo>
+#include <functional>
+#include "aux.hh"
+#include "cadac_constants.hh"
+#include "datadeck.hh"
+#include "module.hh"
+#include "numerical_constants.hh"
+#include "propulsion.hh"
+#include "vehicle.hh"
+class Propulsion;
+
+class AeroDynamics : public Dynamics
+{
+    TRICK_INTERFACE(AeroDynamics);
+
+public:
+    explicit AeroDynamics();
+
+    void load_aerotable(const char *filename);
+
+    virtual void init(LaunchVehicle *VehicleIn){};
+    virtual void algorithm(LaunchVehicle *VehicleIn);
+
+private:
+    Datadeck aerotable; /* ** (--) Aero Deck */
+};
+
+#endif  // __AERODYNAMICS_HH__
