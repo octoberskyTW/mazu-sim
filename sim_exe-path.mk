c496b054bc49ca6752c5552ec899e53250f7f3ce
j8xixo12 <as2266317@gmail.com>
2020-02-18 13:39:09 +0800

    remove sheipa & sample build from build list

--- sim_exe-path.mk
+++ sim_exe-path.mk
@@ -1,16 +1,12 @@
 
-ifeq ($(project), $(filter $(project), sample all))
-SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sample_master/
-SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sample_slave/
-endif
+# ifeq ($(project), $(filter $(project), sample all))
+# SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sample_master/
+# SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sample_slave/
+# endif
 
 ifeq ($(project), $(filter $(project), skyline all))
 SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/skyline_dm/
 SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/skyline_gnc/
 endif
 
-ifeq ($(project), $(filter $(project), sheipa all))
-SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sheipa_dm/
-SIM_EXE_TRICK_PATH+=$(TOP_DIR)/sim_exe/sheipa_gnc/
-endif
 $(info SIM_EXE_TRICK_PATH = $(SIM_EXE_TRICK_PATH))
